a78f57a56268da96ea13d7e6c903068d9e273c03
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-06-04 14:57:35 -0400

    Commit before parting

---
+++ src/dancersGoToMouse.js
@@ -0,0 +1,10 @@
+document.addEventListener('click', function(ev){
+  const x = ev.clientY;
+  const y = ev.clientX;
+  console.log(x, y);
+  // call function to move to x, y
+}, false);
+
+const dancerGoToPosition = function(x, y) {
+
+};
