f14ee75889bbc3d0cdea545b00c905e2dcd70383
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-04 12:31:25 -0700

    Makes dancers cordinates equal click

--- src/dancersGoToMouse.js
+++ src/dancersGoToMouse.js
@@ -1,10 +1,20 @@
-document.addEventListener('click', function(ev){
+document.addEventListener('click', function(ev) {
   const x = ev.clientY;
   const y = ev.clientX;
-  console.log(x, y);
   // call function to move to x, y
+  if (x > 40) {
+    dancerGoToPosition(x, y);
+  }
 }, false);
 
 const dancerGoToPosition = function(x, y) {
+  const dancers = Array.from(document.querySelectorAll('.dancer'));
+  // let top = 50;
+  var x = x.toString();
+  var y = y.toString();
 
+  for (let dancer of dancers) {
+    dancer.style.top = x + 'px';
+    dancer.style.left = y + 'px';
+  }
 };
